fa06c02205601126dd284355add7a467a4f98715
Marco <marcocelaya34@gmail.com>
2020-07-06 01:29:44 -0500

    Terver Commit

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,20 @@
 //PUERTO
 
-process.env.PORT = process.env.PORT || 3000;
+process.env.PORT = process.env.PORT || 3005;
+
+//ENTORNO
+
+process.env.NODE_ENV = process.env.NODE_ENV || "dev";
+
+//BASE DE DATOS
+
+let urlDB;
+
+if (process.env.NODE_ENV == "dev") {
+  urlDB = "mongodb://localhost:27017/cafe";
+} else {
+  urlDB =
+    "mongodb+srv://marcocelaya34:hahopeadyochc4@cafe.w46cm.gcp.mongodb.net/test";
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ require("./config/config");
 const express = require("express");
 const app = express();
 
+const mongoose = require("mongoose");
+
 const bodyParser = require("body-parser");
 
 // parse application/x-www-form-urlencoded
@@ -11,36 +13,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.get("/usuario", function (req, res) {
-  res.json("get Usuario");
-});
+app.use(require("./routes/usuario"));
 
-app.post("/usuario", function (req, res) {
-  let body = req.body;
+mongoose.connect(
+  process.env.URLDB ,
+  { useNewUrlParser: true, useCreateIndex: true },
+  (err, res) => {
+    if (err) throw err;
 
-  if (body.nombre == undefined) {
-    res.status(400).json({
-      ok: false,
-      mensaje: "El nombre es necesario",
-    });
-  } else {
-    res.json({
-      persona: body,
-    });
+    console.log("Base de Datos Online");
   }
-});
+);
 
-app.put("/usuario/:id", function (req, res) {
-  let id = req.params.id;
-
-  res.json({
-    id,
-  });
-});
-
-app.delete("/usuario", function (req, res) {
-  res.json("Delete Usuario");
-});
 app.listen(process.env.PORT, () => {
   console.log("Escuchando puerto: ", process.env.PORT);
 });
