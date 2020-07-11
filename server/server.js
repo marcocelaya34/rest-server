2b81057ec46bd40abbe3b86d832dba8bc1323924
Marco <marcocelaya34@gmail.com>
2020-07-10 19:16:40 -0500

    COR fix it

--- server/server.js
+++ server/server.js
@@ -11,7 +11,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
-
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(require("./routes/usuario"));
 
 async function connectDB() {
