ed5bcada8054d3731948ab5e0cc4d9243913d5b4
Marco <marcocelaya34@gmail.com>
2020-07-12 16:35:44 -0500

    otro commit

--- server/server.js
+++ server/server.js
@@ -11,14 +11,28 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
-app.use(function (req, res, next) {
+
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+
+  // authorized headers for preflight requests
+  // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
+
+  app.options("*", (req, res) => {
+    // allowed XHR methods
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, PATCH, PUT, POST, DELETE, OPTIONS"
+    );
+    res.send();
+  });
 });
+
 app.use(require("./routes/usuario"));
 
 async function connectDB() {
